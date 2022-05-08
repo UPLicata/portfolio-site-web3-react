01fe15ba1dccfd2e10342b043e3fc1169caab11e
Paul Licata <paul@licata.com>
2022-05-08 19:43:26 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
 
             <div className='header__content'>
                 <h1>Paul Licata</h1>
-                <p>Blockchain & Cryptocurrentcy Enthusiast</p>
-                <button className='button'>Get In Touch</button>
+                <p>Blockchain & Cryptocurrency Enthusiast</p>
+                <a href="mailto:paul@licata.com?subject=Mail from Portfolio Website" className="button">Get In Touch</a>
             </div>
         </section>
     );
