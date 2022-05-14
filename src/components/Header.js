371772b616e287220293e00d21720bb60c3ae47c
Paul Licata <paul@licata.com>
2022-05-14 19:36:42 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+// Import Assets
+import profile from '../assets/profile.png';
+
+const Header = () => {
+    return (
+        <section className='header'>
+            <img src={profile} alt="Paul Licata" />
+
+            <div className='header__content'>
+                <h1>Paul Licata</h1>
+                <p>Blockchain & Cryptocurrency Enthusiast </p>
+                <a href="mailto:paul@licata.com?subject=Mail from Portfolio Website" className="button">Get In Touch</a>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
